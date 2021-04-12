52f592604ead975f5f4c11a242dcf872d3aeb484
ProgGeddy <geddy.israel@gmail.com>
2021-04-11 23:56:42 -0600

    Add lint-staged to only lint staged changes during commits

--- src/app.js
+++ src/app.js
@@ -1,13 +1,15 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
+
 
 const App = () => {
   return (
     <div>
       <h1>Hello World!</h1>
-      				<h3>From inside src/app.js, which is bundled to public/bundle.js</h3>
- 	</div>
+      <h3>From inside src/app.js, which is bundled to public/bundle.js</h3>
+    </div>
   );
 };
+
+import React from 'react';
+import ReactDOM from 'react-dom';
 
 ReactDOM.render(<App />, document.getElementById('root'));
