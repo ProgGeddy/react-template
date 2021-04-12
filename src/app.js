9d95f4997018320f21890a91ee1638b40537edde
ProgGeddy <geddy.israel@gmail.com>
2021-04-11 23:41:32 -0600

    Testing Husky Linting and Formatting

--- src/app.js
+++ src/app.js
@@ -5,7 +5,7 @@ const App = () => {
   return (
     <div>
       <h1>Hello World!</h1>
-      			<h3>From inside src/app.js, which is bundled to public/bundle.js</h3>
+      <h3>From inside src/app.js, which is bundled to public/bundle.js</h3>
     </div>
   );
 };
