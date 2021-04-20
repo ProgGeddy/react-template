a09ca2bfe65461afb8f82ac098a8de1a6b0b45bb
ProgGeddy <geddy.israel@gmail.com>
2021-04-19 23:09:38 -0600

    Removing Husky and Lint-Staged

--- src/app.js
+++ src/app.js
@@ -4,13 +4,20 @@ import ReactDOM from 'react-dom';
 const App = () => {
   return (
     <div>
-      <h1>Hello World!</h1>
-      <h3>From inside src/app.js, which is bundled to public/bundle.js</h3>
+  <h1>Hello World!</h1>
+  <h3>From inside src/app.js, which is bundled to public/bundle.js</h3>
     </div>
   );
 };
 
 ReactDOM.render(<App />, document.getElementById('root'));
+
+
+
+
+
+
+
 
 
 
