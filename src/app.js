ed8e19013a0e3e2e435834fc5561a9dda4a4a34a
ProgGeddy <geddy.israel@gmail.com>
2021-04-19 23:17:19 -0600

    Testing using pretty quick and husky

--- src/app.js
+++ src/app.js
@@ -7,7 +7,7 @@ const App = () => {
   <h1>Hello World!</h1>
   <h3>From inside src/app.js, which is bundled to public/bundle.js</h3>
     </div>
-  );
+    );
 };
 
 ReactDOM.render(<App />, document.getElementById('root'));
